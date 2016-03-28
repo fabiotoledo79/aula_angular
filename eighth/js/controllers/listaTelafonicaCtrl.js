e70aa218b08a51ff8da75cfd87212ed7940ad4b2
F@by23151764 <fabiotoledo79@yahoo.com.br>
2016-03-27 22:16:42 -0300

    eighth class

---
+++ eighth/js/controllers/listaTelafonicaCtrl.js
@@ -0,0 +1,39 @@
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, contatosAPI, operadorasAPI, serialGenerator){
+  $scope.app = "Lista Telefonica";
+  $scope.contatos = [];
+  $scope.operadoras = [];
+  var carregarContatos = function () {
+    contatosAPI.getContatos().success(function (data) {
+      $scope.contatos = JSON.parse(data);
+    }).error(function (data, status) {
+      $scope.message = "Aconteceu um problema: " + data;
+    });
+  };
+  var carregarOperadoras = function () {
+    operadorasAPI.getOperadoras().success(function (data) {
+      $scope.operadoras = JSON.parse(data);
+    });
+  };
+  $scope.adicionarContato = function(contato){
+    contato.serial = serialGenerator.generate();
+    $scope.contatos.push(contato);
+    delete $scope.contato;
+    $scope.contatosForm.$setPristine();
+  };
+  $scope.apagarContatos = function(contatos){
+    $scope.contatos = contatos.filter(function(contato){
+      if (!contato.selecionado) return contato;
+    });
+  };
+  $scope.isContatoSelecionado = function(contatos){
+    return contatos.some(function(contato){
+      return contato.selecionado;
+    });
+  };
+  $scope.ordernarCampo = function(campo){
+    $scope.ordernar = campo;
+    $scope.contrario = !$scope.contrario;
+  };
+  carregarContatos();
+  carregarOperadoras();
+});
